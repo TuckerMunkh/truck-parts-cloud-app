db97c129aed98dd53c674eb18ff16c6f3c9b138b
Tucker863 <turtogtoh.m@gmail.com>
2026-04-30 11:11:13 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 st.set_page_config(page_title="Truck Parts System", layout="wide")
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 BUCKET = "part-photos"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
